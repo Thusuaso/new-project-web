d55de0b2fdaf117b7e7a8723473c4cd12b284f17
ferdiinan <inan79@gmail.com>
2023-04-13 11:52:56 +0300

    'l'

--- src/service/SocketService.js
+++ src/service/SocketService.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 const socket = {
-  siparis: io('http://localhost:5001',{ origins: '*:*'}),
+ // siparis: io('http://localhost:5001',{ origins: '*:*'}),
 
-  //siparis: io('https://doktor-socket.mekmar.com',{ origins: '*:*'}),
+  siparis: io('https://doktor-socket.mekmar.com',{ origins: '*:*'}),
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
